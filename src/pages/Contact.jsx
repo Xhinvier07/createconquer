421e7dce69e41455b1514dd263496fc374044761
Shin <jansendeguzman15@gmail.com>
2025-05-06 10:47:19 +0800

    map changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -339,7 +339,7 @@ const Contact = () => {
                 transition={{ duration: 0.6, delay: 0.2 }}
               >
                 <iframe 
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3861.1714589108097!2d120.98958831483992!3d14.592484389815638!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397ca27759cb0ed%3A0x60dcef2bcd8c4c71!2sFEU%20Institute%20of%20Technology!5e0!3m2!1sen!2sph!4v1652841012578!5m2!1sen!2sph" 
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.983097351928!2d120.9884884!3d14.6042791!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f4!3m3!1m2!1s0x3397ca27759cb0ed%3A0x60dcef2bcd8c4c71!2sFEU%20Institute%20of%20Technology!5e0!3m2!1sen!2sph!4v1698765432123!5m2!1sen!2sph" 
                   width="600" 
                   height="450" 
                   style={{ border: 0 }} 
